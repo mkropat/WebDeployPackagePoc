522f0d1fb1a9cb154ebd9a3866edd5d5e73c6de9
Michael Kropat <mail@michael.kropat.name>
2015-05-11 10:49:36 -0400

    New Web Forms+Web Application project

---
+++ WebDeployPackagePoc/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebDeployPackagePoc.Startup))]
+namespace WebDeployPackagePoc
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
